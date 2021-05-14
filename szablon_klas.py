1feb59e83ad3651fd62e0d4488eedbaae5ce497d
Mateusz Kacprzak <matkac98@gmail.com>
2021-05-14 09:27:27 +0200

    Stworzenie szablonu klas

---
+++ szablon_klas.py
@@ -0,0 +1,35 @@
+class Element:
+    def __init__(self):
+        pass
+    def setVal(self,val):
+        pass
+    def delVal(self):
+        pass
+    def getVal(self):
+        pass
+
+class Board:
+    def __init__(self):
+        pass
+    def randomizeElement(self):
+        pass
+    def right(self):
+        pass
+    def left(self):
+        pass
+    def up(self):
+        pass
+    def down(self):
+        pass
+    def getTable(self):
+        pass
+    def copyTable(self):
+        pass
+
+class Game:
+    def __init__(self):
+        pass
+    def draw(self,table):
+        pass
+    def run(self, board):
+        pass
